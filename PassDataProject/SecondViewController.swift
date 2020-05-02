7524e35a18902dc3a63a396e7d31a8727b78b8c5
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-02 21:15:20 +0300

    First commit

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Volodymyr Boichuk on 02.05.2020.
+//  Copyright © 2020 Volodymyr Boichuk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+}
+
+   
+
+
---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Volodymyr Boichuk on 02.05.2020.
+//  Copyright © 2020 Volodymyr Boichuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBAction func logginTapped(_ sender: UIButton) {
+    }
+}
